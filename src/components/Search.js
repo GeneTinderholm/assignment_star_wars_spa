469967d9441c63c30415dcce752929ee1dbb0d5e
GeneTinderholm <tinderholmgene@gmail.com>
2018-01-04 13:06:32 -0600

    lunch

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,30 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.state = {
-      display: {}
+      display: {},
     };
   }
-  componentDidMount() {
+  componentWillUpdate() {
     fetch(
       `https://swapi.co/api/${this.props.match.params.search}/?page=${
         this.props.match.params.page
-      }`
+      }`,
     )
       .then(response => response.json())
       .then(json => {
-        this.setState({ display: json });
+        this.setState({display: json});
       });
   }
 
   display(json) {
     if (json.results) {
       switch (this.props.match.params.search) {
-        case "people":
+        case 'people':
           return json.results.map(item => {
             return (
               <ul>
@@ -34,7 +35,7 @@ class Search extends Component {
               </ul>
             );
           });
-        case "planets":
+        case 'planets':
           return json.results.map(item => {
             return (
               <ul>
@@ -47,7 +48,7 @@ class Search extends Component {
               </ul>
             );
           });
-        case "films":
+        case 'films':
           return json.results.map(item => {
             return (
               <ul>
@@ -60,7 +61,7 @@ class Search extends Component {
               </ul>
             );
           });
-        case "species":
+        case 'species':
           return json.results.map(item => {
             return (
               <ul>
@@ -73,7 +74,7 @@ class Search extends Component {
               </ul>
             );
           });
-        case "species":
+        case 'species':
           return json.results.map(item => {
             return (
               <ul>
@@ -83,6 +84,36 @@ class Search extends Component {
                 <li>{item.average_height}</li>
                 <li>{item.skin_colors}</li>
                 <li>{item.hair_colors}</li>
+              </ul>
+            );
+          });
+        case 'vehicles':
+          return json.results.map(item => {
+            return (
+              <ul>
+                <li>{item.name}</li>
+                <li>{item.model}</li>
+                <li>{item.manufacturer}</li>
+                <li>{item.cost_in_credits}</li>
+                <li>{item.length}</li>
+                <li>{item.max_atmosphering_speed}</li>
+                <li>{item.crew}</li>
+                <li>{item.passengers}</li>
+              </ul>
+            );
+          });
+        case 'starships':
+          return json.results.map(item => {
+            return (
+              <ul>
+                <li>{item.name}</li>
+                <li>{item.model}</li>
+                <li>{item.manufacturer}</li>
+                <li>{item.cost_in_credits}</li>
+                <li>{item.length}</li>
+                <li>{item.max_atmosphering_speed}</li>
+                <li>{item.crew}</li>
+                <li>{item.passengers}</li>
               </ul>
             );
           });
@@ -94,10 +125,42 @@ class Search extends Component {
     }
   }
 
+  pagination() {
+    let pageArray = [];
+    if (this.state.display.next) {
+      pageArray.push(
+        <a href={`./${Number(this.props.match.params.page) +
+            1}`}>
+          {' '}
+          Next{' '}
+        </a>,
+      );
+    }
+    pageArray.push(
+      <a href={`./${Number(this.props.match.params.page)}`}>
+        {' '}
+        {this.props.match.page}{' '}
+      </a>,
+    );
+    if (this.state.display.previous) {
+      pageArray.push(
+        <a href={`./${Number(this.props.match.params.page) -
+            1}`}>
+          {' '}
+          Previous{' '}
+        </a>,
+      );
+    }
+
+    return <div>{pageArray}</div>;
+  }
+
   render() {
     if (this.state.display) {
       //Remember --> you cannot render objects just like that! So, we use JSON.stringify here
-      return <div>{this.display(this.state.display)}</div>;
+      return <div>{this.display(this.state.display)}
+      {this.pagination()}
+      </div>;
     } else {
       return <div>Loading...</div>;
     }
